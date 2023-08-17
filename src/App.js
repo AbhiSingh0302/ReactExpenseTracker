458ec8ee6ca12584dcef728ac0948fa6c5da457c
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-08-17 13:50:43 +0530

    dynamically pass data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      date: new Date(2023, 7, 17),
+      title: 'Car Insurance',
+      amount: 294.67,
+      location: 'India'
+    }
+  ]
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expenses[0].title}
+       date={expenses[0].date}
+       amount={expenses[0].amount} 
+       location={expenses[0].location}></ExpenseItem>
     </div>
   );
 }
