3a89eeada395476fbac03310d160571a5d2b667f
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-08-17 10:35:49 +0530

    styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,21 @@
+import './ExpenseItem.css';
+
 function ExpenseItem(){
+    const expenseDate = new Date(2023,7,17);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+    const LocationOfExpenditure = "India";
+
     return (
-        <div>
-            <h2>Expense Items</h2>
-            <p>Food Rs 10</p>
-            <p>Petrol Rs 100</p>
-            <p>Movies Rs 200</p>
+        <div className='expense-item'>
+            <div>{expenseDate.toISOString()}</div>
+            {/* <div className='expense-item__desription'> */}
+            <h2>{expenseTitle}</h2>
+            <h2>{LocationOfExpenditure}</h2>
+            <div className='expense-item__price'>Rs{expenseAmount}</div>
+            {/* </div> */}
         </div>
-    )
+    );
 }
 
 export default ExpenseItem;
