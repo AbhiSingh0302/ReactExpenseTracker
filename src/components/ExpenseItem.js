f67fb012864725d6334050aa469b84ae7fec62e0
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-08-17 14:57:42 +0530

    multiple components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,13 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 
 function ExpenseItem(props){
-    // const expenseDate = new Date(2023,7,17);
-    // const expenseTitle = 'Car Insurance';
-    // const expenseAmount = 294.67;
-    // const LocationOfExpenditure = "India";
 
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
-            {/* <div className='expense-item__desription'> */}
-            <h2>{props.title}</h2>
-            <h2>{props.location}</h2>
-            <div className='expense-item__price'>Rs{props.amount}</div>
-            {/* </div> */}
+            <ExpenseDate date={props.date}/>
+            <ExpenseDetails title={props.title} location={props.location} amount={props.amount}/>
         </div>
     );
 }
