78fdd95321bf940e8af563f9a135d95b211a04ed
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-08-18 08:10:55 +0530

    manipulating states

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,18 @@
-import React from "react"; // imported just for learning purpose and putting emphasis on it
+import React,{useState} from "react"; // imported just for learning purpose and putting emphasis on it
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+    const [title,setTitle] = useState(props.title);
+    const [amount,setAmount] = useState(props.amount);
     const clickHandler = () => {
-        console.log("Clicked!!!");
+        setTitle("Updated!!")
+        console.log(title);
+    }
+    const changePrice = () => {
+        setAmount(100)
     }
     const removeHandler = (e) => {
         console.log(e.target.parentNode.remove());
@@ -15,9 +21,10 @@ const ExpenseItem = (props) => {
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
-            <ExpenseDetails title={props.title} location={props.location} amount={props.amount}/>
+            <ExpenseDetails title={title} location={props.location} amount={amount}/>
             <button onClick={clickHandler}>Change Title!!</button>
             <button onClick={removeHandler}>Delete</button>
+            <button onClick={changePrice}>Change Price</button>
         </Card>
     );
 }
