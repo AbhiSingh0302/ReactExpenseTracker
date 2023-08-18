d4fe198641badc1364c72d70894075c017cb83dc
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-08-18 11:51:42 +0530

    onchange event handler

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,7 +1,12 @@
 const ExpenseForm = () => {
+    const changeHandler = (e) => {
+        console.log(e.target.value);
+    }
+
+
     return (
         <form>
-            <input type="text" placeholder="Enter title"/>
+            <input type="text" placeholder="Enter title" onChange={changeHandler}/>
             <input type="number" placeholder="Enter amount"/>
             <input type="date" placeholder="Enter date"/>
             <input type="submit" placeholder="Add"/>
