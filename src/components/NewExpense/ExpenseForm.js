ccd06986e7df571b9110b45f3b8aa146f162778b
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-08-20 23:08:02 +0530

    show and hide expense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,6 +6,7 @@ const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState("");
     const [enteredAmount, setEnteredAmount] = useState("");
     const [enteredDate, setEnteredDate] = useState("");
+    const [formElement,setFormElement] = useState();
 
     const titleChangeHandler = (e) => {
         setEnteredTitle(e.target.value);
@@ -30,27 +31,46 @@ const ExpenseForm = (props) => {
         setEnteredAmount("");
         setEnteredTitle("");
     }
+
+    const closeForm = () => {
+        setFormElement();
+        document.getElementById('formBtn').style.display = 'block';
+    }
+
+    const openForm = (e) => {
+        setFormElement(expForm);
+        console.log(document.getElementById('formBtn'))
+        document.getElementById('formBtn').style.display = 'none';
+    }
+
+    const expForm = <form onSubmit={submitHandler}>
+    <div className="new-expense__controls">
+        <div className="new-expense__control">
+            <label>Title</label>
+            <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
+        </div>
+        <div className="new-expense__control">
+            <label>Amount</label>
+            <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler}/>
+        </div>
+        <div className="new-expense__control">
+            <label>Date</label>
+            <input type="date" value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+        </div>
+    </div>
+    <div className="new-expense__actions">
+        <button type="submit"> Add Expense </button>
+        <button onClick={closeForm}> Cancel </button>
+    </div>
+</form>
     
     return (
-        <form onSubmit={submitHandler}>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
-                    <label>Title</label>
-                    <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
-                </div>
-                <div className="new-expense__control">
-                    <label>Amount</label>
-                    <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler}/>
-                </div>
-                <div className="new-expense__control">
-                    <label>Date</label>
-                    <input type="date" value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
-                </div>
+        <div>
+            {formElement}
+            <div className="new-expense__actions" id="formBtn">
+                <button onClick={openForm}>Add Expense</button>
             </div>
-            <div className="new-expense__actions">
-                <button type="submit"> Add Expense </button>
-            </div>
-        </form>
+        </div>
     )
 }
 
